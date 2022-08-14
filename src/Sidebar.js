24a2522cf4bc3e4ef98fd93729793d65f5f52330
Ali <ali.naqvi.dev@gmail.com>
2022-08-14 16:40:59 -0500

    changes made: styling condensed and nav bar is half the size with responsive items

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,5 @@
 import { FaBaby, FaFire, FaHome, FaQuestion, FaSearch } from 'react-icons/fa'
-
+import src from "./Stub-logos_white.png"
 const SideBarIcon = ({ icon }) => {
   <div className="sidebar-icon">
     {icon}
@@ -7,11 +7,13 @@ const SideBarIcon = ({ icon }) => {
 }
 
 const SideBar = () => {
+ 
   return (
-    <div className="fixed top-0 left-0 h-20 w-screen m-0 flex flex-row justify-center bg-gray-400  text-cyan-500 shadow-xl">
-      <div className='flex flex-row space-x-4'>
-        <input className='h-10 w-auto mt-4'  />
-        <div className="sidebar-icon cursor-pointer mt-3">
+    <div className="fixed top-0 left-0 h-16 w-screen m-0 flex flex-row justify-center bg-gray-400  text-cyan-500 shadow-xl">
+        <img src={src} className="h-56 fixed -mt-20 left-10"  />
+      <div className='flex flex-row space-x-4 w-4/12'>
+        <input className='h-10 w-full mt-3 rounded-md outline-none px-4 text-black'  />
+        <div className="sidebar-icon cursor-pointer h-10 w-12 mt-3">
           <FaSearch size={28} />
         </div>
       </div>
