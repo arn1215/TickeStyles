ba7ff121b8ca16534e1ff6d4a8e482d49a3cefa7
Ali <ali.naqvi.dev@gmail.com>
2022-08-14 14:31:26 -0500

    sidebar init

---
+++ src/Sidebar.js
@@ -0,0 +1,16 @@
+const SideBar = () => {
+  return (
+    <div className = "fixed top-0 left-0 h-screen w-20 m-0
+                      flex flex-col bg-green-300 shadow-lg">
+        <i>a</i>
+        <i>a</i>
+        <i>a</i>
+        <i>a</i>
+        <i>a</i>
+        <i>a</i>
+    </div>
+  )
+}
+
+
+export default SideBar;
