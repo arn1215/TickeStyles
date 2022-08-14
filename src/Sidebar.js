06414effa29c6bab562c9afa870c9b866b6853e9
Ali <ali.naqvi.dev@gmail.com>
2022-08-14 18:00:53 -0500

    saving nav

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,7 +12,7 @@ const SideBar = () => {
     <div className="fixed top-0 left-0 h-16 w-screen m-0 flex flex-row justify-center bg-gray-400  text-cyan-500 shadow-xl">
         <img src={src} className="h-56 fixed -mt-20 left-10"  />
       <div className='flex flex-row space-x-4 w-4/12'>
-        <input className='h-10 w-full mt-3 rounded-md outline-none px-4 text-black'  />
+        <input className='h-10 w-full mt-3 rounded-md outline-none px-4 text-black shadow-lg'  />
         <div className="sidebar-icon cursor-pointer h-10 w-12 mt-3">
           <FaSearch size={28} />
         </div>
